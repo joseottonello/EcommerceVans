ecb98a4b4cea73cdf1a7659e05cb71732a747d8d
José Ottonello <dev.ottonellojose@gmail.com>
2022-08-29 18:39:11 -0300

    fixed responsive step 2

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -44,40 +44,43 @@ const Cart = () => {
 
     return (
         <div className="cart-container">
-            { cart.map((item) => 
-                <section 
-                key={item.id} 
-                className="cart-container-listProducts">
-                    <img 
-                    src={item.image} 
-                    alt={item.name}
-                    className="cart-container-listProducts-image"/>
-                    <article className="cart-container-text">
-                        <h2>{item.name}</h2>
-                        <p> Productos seleccionados: {item.cantidad} </p>
-                        <p> Valor unitario: ${item.price} </p>
-                        <p> Valor total: ${item.price * item.cantidad} </p>
-                    </article>
-                    <article className="cart-container-action">
-                        <Link  to="/"><Button  variant="outlined" color="error" startIcon={<ArrowBackIosIcon />}>Inicio</Button></Link>
-                        <Button  variant="contained" color="error" onClick={() => removeToCart(item.id)} startIcon={<DeleteIcon />}>Eliminar</Button>
-                    </article>
-                </section>)
-            }
-            <section className="cart-container-total">
+            <section className="cart-products">
+                {
+                    cart.map((item) => 
+                        <div 
+                        key={item.id} 
+                        className="cart-product">
+                            <img 
+                            src={item.image} 
+                            alt={item.name}
+                            className="cart-image"/>
+                            <article className="cart-text">
+                                <h2>{item.name}</h2>
+                                <p> Productos seleccionados: {item.cantidad} </p>
+                                <p> Valor unitario: ${item.price} </p>
+                                <p> Valor total: ${item.price * item.cantidad} </p>
+                            </article>
+                            <article className="cart-action">
+                                <Link  to="/"><Button  variant="outlined" color="error" startIcon={<ArrowBackIosIcon />}>Inicio</Button></Link>
+                                <Button  variant="contained" color="error" onClick={() => removeToCart(item.id)} startIcon={<DeleteIcon />}>Eliminar</Button>
+                            </article>
+                        </div>)
+                }
+            </section>
+            <section className="cart-total">
                 {
                     totalCart() === 0
-                    ? <div className="cart-container-total-alert">
-                        <Alert variant="filled" severity="error">¡Parece que no hay nada en el Carrito!</Alert>
-                        <Link  to="/"><Button variant="contained" color="error">Volver al inicio</Button></Link>
-                    </div>
-                    : <section className="cart-container-total-finalizar">
-                        <h1>Total a pagar: ${Number(totalCart().toFixed(2))}</h1>
-                        <div className="cart-container-total-finalizar-action">
-                            <Button variant="outlined" color="error" startIcon={<ShoppingBagIcon/>} onClick={handleBuyOrderInCart}>Realizar compra</Button>
-                            <Button variant="contained" color="error" startIcon={<DeleteIcon />} onClick={clearCart}>vaciar carrito</Button>
+                        ? <div className="cart-total-alert">
+                            <Alert variant="filled" severity="error">¡Parece que no hay nada en el Carrito!</Alert>
+                            <Link  to="/"><Button variant="contained" color="error">Volver al inicio</Button></Link>
                         </div>
-                      </section>
+                        : <section className="cart-total-finalizar">
+                            <h1>Total a pagar: ${Number(totalCart().toFixed(2))}</h1>
+                            <div className="cart-total-finalizar-action">
+                                <Button variant="outlined" color="error" startIcon={<ShoppingBagIcon/>} onClick={handleBuyOrderInCart}>Realizar compra</Button>
+                                <Button variant="contained" color="error" startIcon={<DeleteIcon />} onClick={clearCart}>vaciar carrito</Button>
+                            </div>
+                        </section>
                 }
             </section>
         </div>
